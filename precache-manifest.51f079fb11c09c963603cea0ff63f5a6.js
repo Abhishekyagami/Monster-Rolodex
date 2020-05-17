ff7a9515f9e6855f854b62ce243e7d0a8ee548e5
Light <abhishekyagami2@gmail.com>
2020-05-18 03:44:45 +0530

    Updates

---
+++ precache-manifest.51f079fb11c09c963603cea0ff63f5a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9574fecf2dbcecc9502c0cfc42540329",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "7f78d2b23d7c801ed1a7",
+    "url": "/Monster-Rolodex/static/css/main.ea2376db.chunk.css"
+  },
+  {
+    "revision": "f1f7019348927741577e",
+    "url": "/Monster-Rolodex/static/js/2.7e22ba48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.7e22ba48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f78d2b23d7c801ed1a7",
+    "url": "/Monster-Rolodex/static/js/main.64c60aba.chunk.js"
+  },
+  {
+    "revision": "e255ba29144c45cab533",
+    "url": "/Monster-Rolodex/static/js/runtime-main.ecdb6645.js"
+  }
+]);
